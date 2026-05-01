7f7d08e591bcb0ce347bce9e3aadaf003cb19707
kuro-spm <sarapratsm@gmail.com>
2026-05-01 20:08:24 +0200

    qualification amb default 0.0

--- models/school_enrollment.py
+++ models/school_enrollment.py
@@ -12,7 +12,7 @@ class SchoolEnrollment(models.Model):
     _name = 'school.enrollment'
     _description = 'School Enrollment Management'
 
-    qualification = fields.Float(digits=(3,1), string="Qualification")
+    qualification = fields.Float(digits=(3,1), string="Qualification", default=0.0)
     student_id = fields.Many2one('school.student', string='Student', required=True)
     subject_ids = fields.One2many('school.enrollment.subject', 'enrollment_id', string='Subjects')
     edition_id = fields.Many2one('school.course.edition', string='Course edition', required=True) 
@@ -31,6 +31,8 @@ class SchoolEnrollment(models.Model):
     @api.constrains('qualification')
     def _check_qualification(self):
         for obj in self:
+            if not obj.qualification:
+                obj.qualification=0
             # S'avalua directament la condició matemàtica.
             # Es recomana posar els decimals (10.0 i 0.0) per mantenir la coherència de tipus Float.
             if obj.qualification > 10.0 or obj.qualification < 0.0:
