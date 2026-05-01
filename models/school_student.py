8c81dda30565098fc12d6cc44160324351e65dd3
kuro-spm <sarapratsm@gmail.com>
2026-05-01 15:42:21 +0200

    Prova reestructuracio view form student

--- models/school_student.py
+++ models/school_student.py
@@ -18,32 +18,23 @@ class SchoolStudent(models.Model):
     birthdate = fields.Date('Birthdate', required=True)
     phone = fields.Char('Phone', required=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other','Other')],'Gender', required=True)
-
     #adress
     address1 = fields.Char('Adress 1', required=True)
     address2 = fields.Char('Adress 2') #no obligatoria
     zip_code = fields.Char('Zip Code', required=True)
     city = fields.Char('City', required=True)
-  
-    country_id = fields.Many2one('res.country','Citizenship', required=True)
     state_id = fields.Many2one('res.country.state', string='State')
-    customer_id = fields.Many2one('res.partner', string="Guardian", required=True) 
-    
-    #Dades optatives:
-    #adress2_id = fields.One2many(...)
-    email = fields.Char('eMail', size=60, required=False)
+    country_id = fields.Many2one('res.country','Citizenship', required=True)
 
+    #El Tutor/Pagador (res.partner)
+    customer_id = fields.Many2one('res.partner', string="Guardian", required=True)   
+    #Dades optatives:
+    email = fields.Char('eMail', size=60, required=False)
     #Camps calculats
     age =fields.Integer('Age', compute='_compute_age', store=False)
-
     #Dades obligatòries segons condició:
-    #vat_number = fields.Char('Vat Number', size=11, required='age>=14') #ES12345678L
-    #tin = fields.Char('Tax ID', size=14, required='age>=14')
-    #guardian_info = fields.Text('Guardian information', required='age<18') #Alternativa: Html.)
-    #Dades obligatòries segons condició (Controlat a la vista XML):
     tin = fields.Char('Tax ID', size=14)
     guardian_info = fields.Text('Guardian information')
-
     #Altres relacions
     enrollment_ids = fields.One2many('school.enrollment', 'student_id', string='Enrollment')
 
